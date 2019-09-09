d8e0ab9402b766066cccf4da154bad944e210531
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-09-09 23:19:41 +0900

    リファクタリング・修正

--- code/tf_idf.py
+++ code/tf_idf.py
@@ -3,13 +3,13 @@ import typing
 import numpy as np
 
 
-def get_all_words(in_csv: str) -> typing.List:
-    all_words = []
+def get_all_docs(in_csv: str) -> typing.List:
+    all_docs = []
     with open(in_csv, encoding='utf-8') as fin:
         for line in fin:
             words = [word.strip() for word in line.split(',')[2:]]
-            all_words.append(words)
-    return all_words
+            all_docs.append(words)
+    return all_docs
 
 
 def create_word_set(all_words: typing.List) -> typing.Set:
@@ -34,36 +34,36 @@ def get_int2word(words_set: typing.Set) -> typing.Dict[int, str]:
     return int2word
 
 
-def calc_tf(all_words: typing.List, word2int: typing.Dict[str, int]) -> np.ndarray:
-    tf_array = np.zeros((len(all_words), len(word2int)))
-    for words_idx, words in enumerate(all_words):
+def calc_tf(all_docs: typing.List, word2int: typing.Dict[str, int]) -> np.ndarray:
+    tf_array = np.zeros((len(all_docs), len(word2int)))
+    for doc_idx, doc in enumerate(all_docs):
         temp_array = np.zeros((1, len(word2int)))
-        for word_idx, word in enumerate(words):
+        for word_idx, word in enumerate(doc):
             if word in word2int:
                 temp_array[0, word_idx] += 1
-        tf_array[words_idx:] = temp_array / np.sum(temp_array)
+        tf_array[doc_idx:] = temp_array / np.sum(temp_array)
     return tf_array
 
 
-def calc_idf(all_words: typing.List, word2int: typing.Dict[str, int]) -> np.ndarray:
+def calc_idf(all_docs: typing.List, word2int: typing.Dict[str, int]) -> np.ndarray:
     temp_array = np.zeros((1, len(word2int)))
-    all_docs = len(all_words)
+    all_docs = all_docs
     for idx, word in enumerate(word2int.keys()):
         word_count = 0
-        for doc in all_words:
+        for doc in all_docs:
             if word in doc:
                 word_count += 1
         temp_array[0, idx] = word_count
-    idf_array = np.log2((all_docs + 1 / temp_array + 1))
+    idf_array = np.log2((len(all_docs) + 1 / temp_array + 1))
     return idf_array
 
 
 def tf_idf(in_csv: str):
-    all_words = get_all_words(in_csv)
-    words_set = create_word_set(all_words)
+    all_docs = get_all_docs(in_csv)
+    words_set = create_word_set(all_docs)
     word2int = get_word2int(words_set)
     int2word = get_int2word(words_set)
-    tf_array = calc_tf(all_words, word2int)
-    idf_array = calc_idf(all_words, word2int)
+    tf_array = calc_tf(all_docs, word2int)
+    idf_array = calc_idf(all_docs, word2int)
     tf_idf = tf_array * idf_array
     return tf_idf
