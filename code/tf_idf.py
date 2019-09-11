df3cf8455a28fb62a779070c559a396896b90f32
NaoY-2501 <solaris2501.fe2@gmail.com>
2019-09-11 17:38:20 +0900

    TF-IDF計算・計算結果出力を実装

--- code/tf_idf.py
+++ code/tf_idf.py
@@ -1,35 +1,40 @@
 import typing
 
 import numpy as np
+import pandas as pd
 
 
 def get_all_docs(in_csv: str) -> typing.List:
     all_docs = []
     with open(in_csv, encoding='utf-8') as fin:
         for line in fin:
-            words = [word.strip() for word in line.split(',')[2:]]
+            words = [word.strip() for word in line.split(',')[3:]]
             all_docs.append(words)
     return all_docs
 
 
-def create_word_set(all_words: typing.List) -> typing.Set:
-    word_set = set()
+def create_words_list(all_words: typing.List) -> typing.List:
+    flatten_words = []
     for words in all_words:
         for word in words:
-            word_set.add(word)
-    return word_set
+            flatten_words.append(word)
+    words_list = []
+    for word in flatten_words:
+        if word not in words_list:
+            words_list.append(word)
+    return words_list
 
 
-def get_word2int(words_set: typing.Set) -> typing.Dict[str, int]:
+def get_word2int(words_list: typing.List) -> typing.Dict[str, int]:
     word2int = {}
-    for idx, word in enumerate(words_set):
+    for idx, word in enumerate(words_list):
         word2int[word] = idx
     return word2int
 
 
-def get_int2word(words_set: typing.Set) -> typing.Dict[int, str]:
+def get_int2word(words_list: typing.List) -> typing.Dict[int, str]:
     int2word = {}
-    for idx, word in enumerate(words_set):
+    for idx, word in enumerate(words_list):
         int2word[idx] = word
     return int2word
 
@@ -38,8 +43,8 @@ def calc_tf(all_docs: typing.List, word2int: typing.Dict[str, int]) -> np.ndarra
     tf_array = np.zeros((len(all_docs), len(word2int)))
     for doc_idx, doc in enumerate(all_docs):
         temp_array = np.zeros((1, len(word2int)))
-        for word_idx, word in enumerate(doc):
-            if word in word2int:
+        for word_idx, word in enumerate(word2int.keys()):
+            if word in doc:
                 temp_array[0, word_idx] += 1
         tf_array[doc_idx:] = temp_array / np.sum(temp_array)
     return tf_array
@@ -58,12 +63,37 @@ def calc_idf(all_docs: typing.List, word2int: typing.Dict[str, int]) -> np.ndarr
     return idf_array
 
 
+def l2_normalize(tf_idf: np.ndarray) -> np.ndarray:
+    l2_norm = np.linalg.norm(tf_idf, ord=2)
+    return tf_idf / l2_norm
+
+
+def get_tf_idf_df(in_csv: str, int2word: typing.Dict[int, str], tf_idf_array: np.ndarray):
+    with open(in_csv, encoding='utf-8') as fin:
+        index = []
+        for line in fin:
+            rows = line.split(',')
+            index.append(f'{rows[0].strip()} {rows[1].strip()}')
+    columns = [int2word[i] for i in range(np.shape(tf_idf_array)[1])]
+    words_tf_idf = pd.DataFrame(tf_idf_array, index=index, columns=columns)
+    return words_tf_idf
+
+
+def show_result(df: pd.DataFrame):
+    with open('result.txt', 'w', encoding='utf-8') as f:
+        for key, row in df.iterrows():
+            top_15 = df.loc[key].sort_values(ascending=False)[:15]
+            f.write(f'{key}\n{top_15}\n{"="*100}\n')
+
+
 def tf_idf(in_csv: str):
     all_docs = get_all_docs(in_csv)
-    words_set = create_word_set(all_docs)
-    word2int = get_word2int(words_set)
-    int2word = get_int2word(words_set)
+    words_list = create_words_list(all_docs)
+    word2int = get_word2int(words_list)
+    int2word = get_int2word(words_list)
     tf_array = calc_tf(all_docs, word2int)
     idf_array = calc_idf(all_docs, word2int)
     tf_idf = tf_array * idf_array
-    return tf_idf
+    normalized_tf_idf = l2_normalize(tf_idf)
+    df = get_tf_idf_df(in_csv, int2word, normalized_tf_idf)
+    show_result(df)
